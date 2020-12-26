10dec3afda533b7f8af0959ff61877d23942d4a5
JaspreetSAujla <jazzyf70@gmail.com>
2020-12-26 16:48:32 +0000

    Added a method for the kitchen.

--- PlayerClass.py
+++ PlayerClass.py
@@ -18,5 +18,15 @@ class Player:
             self.escaped = This will track whether the player is allowed to escape or not.
                            All the essential tasks need to be completed to escape.
                            Set to False by default.
+
+            self.power = This will check whether the player has turned on the power.
+                         Power needs to be turned on for value to become True.
+                         Set to False by default.
+            
+            self.batteries = Checks whether the player has picked up the batteries.
+                             Batteries can be used in the radio.
+                             Set to False by default.
         """
         self.escaped = False
+        self.power = False
+        self.batteries = False
--- RoomClass.py
+++ RoomClass.py
@@ -11,24 +11,30 @@ class Rooms:
     
     Methods:
         __init__ = initialises the instance of the class.
+                   Takes a default argument of 'living'.
 
         Move = Allows the player to navigate through the house.
+
+        LivingRoom = Runs the code for when the player enters the living room.
+
+        Kitchen = Runs the code for when the player enters the kitchen.
     """
 
     def __init__(self, RoomName="living"):
         """
         This method defines the initial variables when an object is instantiated.
 
-        Parameters:
-        self.room_name = Takes the name of the room the character is in. 
-                         This will be set to the living room by default.
+        Variables:
+            self.room_name = Takes the name of the room the character is in. 
+                             This will be set to the living room by default.
 
-        self.objects = Creates an instance of the object class.
-                       This populates the rooms with non-essential objects.
+            self.objects = Creates an instance of the object class.
+                           This populates the rooms with non-essential objects.
         """
         self.room_name = RoomName
         self.objects = Objects()
     
+
 
     def Move(self):
         """
@@ -36,14 +42,14 @@ class Rooms:
         The user can only move to rooms next to their current room.
         This is handled by using an 'if' statement.
         """
-        # Set to False to ensure loop runs at least once when function is called.
-        valid_room = False
-
         # This block is used to move into the living room.
         if self.room_name == "living" or self.room_name == "Living":
-            valid_room = True
             self.LivingRoom()
+        # This block is used to move into the kitchen.
+        elif self.room_name == "Kitchen" or self.room_name == "kitchen":
+            self.Kitchen()
     
+
 
     def LivingRoom(self):
         """
@@ -76,7 +82,7 @@ class Rooms:
             print("What would you like to do?")
             print("Turn on the TV, or move rooms?")
             option = input("(TV/Move) \n")
-            # This block allows the playee to interact with the TV.
+            # This block allows the player to interact with the TV.
             if option == "TV" or option == "Tv" or option == "tv":
                 pass
             # Allows player to move rooms.
@@ -93,6 +99,60 @@ class Rooms:
             self.room_name = input()
             # If statement checks the name is valid to escape the while loop.
             if self.room_name == "Kitchen" or self.room_name == "kitchen" or self.room_name == "Upstairs" or self.room_name == "upstairs" or self.room_name == "Door" or self.room_name == "door":
+                valid_move = True
+            else:
+                print("Not a valid room, try again.")
+    
+
+
+    def Kitchen(self):
+        """
+        This method defines the actions taken when the player enters the kitchen.
+        The player can interact with objects and move rooms.
+
+        Variables:
+            move_rooms = This variables allows the player to interact with the objects as many times
+                         as they want before moving rooms.
+                         Set to False by default.
+
+            option = The option picked by the player is stored in this variable.
+                     Set to None as default.
+
+            valid_move = Checks if the room entered for moving is valid or not.
+                         Set to False as default.
+        """
+        move_rooms = False
+        option = None
+        valid_move = False
+
+        # Code to describe the room to the player.
+        print("You are in the kitchen.")
+        print("There is a radio on the table.")
+        print("There are stairs leading down to the basement and the living room is next to you.")
+
+        # While loop allows player to interact with objects as many times as they like.
+        while move_rooms == False:
+            print("What would you like to do?")
+            print("Turn on the radio or move rooms?")
+            option = input("(Radio/Move) \n")
+            # This block allows player to interact with the radio.
+            if option == "Radio" or option == "radio":
+                pass
+            # Allows player to move rooms.
+            elif option == "Move" or option == "move":
+                move_rooms = True
+            # Prompts player to put a valid input.
+            else:
+                print("Not a valid input, try again.")
+        
+        # Loops over if room is not valid.
+        while valid_move == False:
+            print("Which room would you like to move to>")
+            print("The basement or living room?")
+            print("(Basement/Living)")
+            self.room_name = input()
+            # If statement checks the name is valid to escape the while loop.
+            if self.room_name == "Basement" or self.room_name == "basement" or self.room_name == "Living" or self.room_name == "living":
                 valid_move = True
             else:
                 print("Not a valid room, try again.")
